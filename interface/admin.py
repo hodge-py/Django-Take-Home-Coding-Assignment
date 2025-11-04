d71440c87c846cebcec59c5478dd04e62842ceaf
hodge-py <105604814+hodge-py@users.noreply.github.com>
2025-11-04 16:47:19 -0700

    c

--- interface/admin.py
+++ interface/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import ProductData  # Import the ProductData model
+
+admin.site.register(ProductData)
+
 # Register your models here.
--- interface/models.py
+++ interface/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class ProductData(models.Model): # Model for product data
+    product_name = models.CharField(max_length=255) # Product name field
+    category = models.CharField(max_length=255) # Category field
+    tags = models.CharField(max_length=255) # Tags field
+    description = models.TextField() # Description field
