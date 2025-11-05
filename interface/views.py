26bacfef687fb719560dd2a243b2a18222063d31
hodge-py <105604814+hodge-py@users.noreply.github.com>
2025-11-04 20:01:23 -0700

    c

--- interface/views.py
+++ interface/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import ProductData
+from .models import ProductData, Tags
 
 
 def index(request):
-    items = ProductData.objects.all()
-    return render(request, "index.html", {"items": items})
+    items = ProductData.objects.distinct().all()
+    cate = ProductData.objects.values_list('category', flat=True).distinct()
+    tags = Tags.objects.values_list('name', flat=True).distinct()
+    return render(request, "index.html", {"items": items, "cate": cate, "tags": tags})
 
 # Create your views here.
